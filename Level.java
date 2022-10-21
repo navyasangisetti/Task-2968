f86d66846322bcdfc4cdb0e6cef6f26a8f62185b
navyasangisetti <113688145+navyasangisetti@users.noreply.github.com>
2022-10-21 17:08:25 +0530

    Add files via upload

---
+++ Level.java
@@ -0,0 +1,16 @@
+package enumerations;
+
+class Level {
+	enum Variable{
+	HIGH,
+	MEDIUM,
+	LOW
+
+}
+	
+public static void main(String args[]) {
+
+Variable result = Variable.MEDIUM;
+System.out.println(result);
+}
+}
---
+++ Navya.java
@@ -0,0 +1,12 @@
+package book;
+
+import java.util.Scanner;
+
+public class Navya {
+      public static void main(String args[]) {
+    	  Scanner input = new Scanner(System.in);
+    	  String name = input . nextLine();
+    	  System.out.println(name);
+    	  input.close();
+      }
+}
